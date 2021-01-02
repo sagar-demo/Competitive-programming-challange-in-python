773ce4a5d57d0b7fc2806a78fe9f487908d80922
Rana  Sagar <64120304+sagar-demo@users.noreply.github.com>
2021-01-02 21:06:25 +0530

    Add files via upload

---
+++ Day3.cpp.py
@@ -0,0 +1,8 @@
+### A chef was cooking something special which reuqired exact 200ml water but he doesn't have 200ml cup he was
+# a 500ml cup and a 300ml cup but that dish has required exactly 200ml water
+# can you solve this problem in python?
+bigCup=500
+smallCup=300
+required=bigCup-smallCup
+print(required)
+
