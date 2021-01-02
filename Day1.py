d33051c53787803d292f69eb0bad70c8a6a21283
Rana  Sagar <64120304+sagar-demo@users.noreply.github.com>
2021-01-02 20:41:49 +0530

    Add files via upload

---
+++ Day1.py
@@ -0,0 +1,6 @@
+## There was a teacher in small town who loves coding and wants to create a programe
+# which prints out whole multiplication table of an entered number for his student
+# can you help to write a programe in python
+num=int(input("enter a number.. "))
+for i in range(1,11):
+    print(num,'x',i,'=',num*i)
